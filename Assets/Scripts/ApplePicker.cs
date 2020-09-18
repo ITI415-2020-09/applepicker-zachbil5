874b163b06cdcdc68f33ad6dec93ad9c4e5b85a2
zachbil5 <55146215+zachbil5@users.noreply.github.com>
2020-09-17 22:11:27 -0400

    V1

---
+++ Assets/Scripts/ApplePicker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplePicker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Set in Inspector")]
+    public GameObject basketPrefab;
+    public int numBaskets = 3;
+    public float basketBottomY = -14;
+    public float basketSpacingY = 2f;
+
+    void Start()
+    {
+        for (int i = 0; i < numBaskets; i++)
+        {
+            GameObject tBasketGO = Instantiate(basketPrefab) as GameObject;
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY);
+            tBasketGO.transform.position = pos;
+        }
+    }
+    public void AppleDestroyed()
+    { // 2
+      //// Destroy all of the falling Apples
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");// 3
+        foreach (GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
+        }
+    }
+}
---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    // Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    // Speed at which the AppleTree moves
+    public float speed = 1f;
+
+    // Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    // Chance that the AppleTree will change di
+    public float chanceToChangeDirection;
+
+    // Rate at which Apples will be instantiate
+    public float secondsBetweenAppleDrop;
+
+    void Start()
+    {
+        // Dropping apples every second
+        Invoke("DropApple", secondsBetweenAppleDrop);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrop);
+    }
+    void Update()
+    {
+        // Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Changing Direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); //right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); //left
+        }
+        else if (Random.value < chanceToChangeDirection)
+        {
+            speed *= -1;
+            
+        }
+    }
+}
